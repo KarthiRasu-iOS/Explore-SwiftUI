57c0a7830fa00e7f9f47e600f74181158a4789a0
Karthi Rasu <karthirasu07@gmail.com>
2024-11-28 22:54:42 +0530

    Initial Commit

---
+++ Explore-SwiftUI/Explore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_SwiftUIApp.swift
+//  Explore-SwiftUI
+//
+//  Created by Karthi Rasu on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Explore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
